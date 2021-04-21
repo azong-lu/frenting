7fa40fbd56c88013ee8cfb84073af7b5b1044391
azong-lu <3482073690@qq.com>
2021-04-21 23:16:37 +0800

    city

--- src/page/choose-city/choosecity.js
+++ src/page/choose-city/choosecity.js
@@ -1,20 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { LeftOutlined } from '@ant-design/icons';
 import StatusBar from 'components/status-bar/index';
-import { acquireDistrict } from 'services/AmapAPI';
+import { observer } from 'mobx-react-lite';
+import { useStore } from 'store/store';
+import cityData from 'utils/cityData'
 import styles from './choosecity.less';
+import { Input } from 'antd';
 
 const ChooseCity = (props) => {
   const [cityList, setCityList] = useState([]);
+  const [newList, setNewList] = useState([]);
+  const { currentLocal, newLocation } = useStore()
+  useEffect(() => {
+    let newArr = []
+    cityData.forEach(item => {
+      const { citys } = item
+      newArr.push(...citys)
+    })
+    newArr = newArr.sort((item1, item2) => {
+      return item1.citysName.localeCompare(item2.citysName)
+    })
+    setCityList(newArr)
+    setNewList(newArr)
+  }, []);
   useEffect(() => {
     const key = 'f09c9da07eeed2b4c43f598e8f00d162';
     // acquireDistrict({ key }).then((res) => {
     // });
-  }, [setCityList]);
+
+  }, []);
+
   const handleBack = () => {
     const { history } = props;
     history.goBack(-1);
   };
+
+  const handleSearch = (e) => {
+    const value = e.target.defaultValue
+    const newArr = cityList.filter(item =>
+      item.citysName.indexOf(value) > -1
+    )
+    setNewList(newArr)
+  }
+
+  const handleCityChoose = (city) => {
+    newLocation(city)
+    handleBack()
+  }
+
   const renderStatusBar = () => {
     return (
       <StatusBar>
@@ -24,21 +57,35 @@ const ChooseCity = (props) => {
     );
   };
 
-  const renderCity = () => (
-    <div>
-      <div>当前城市</div>
-      <div></div>
-      <div>热门城市</div>
-      <div></div>
-    </div>
-  );
+  const renderCity = () => {
+    return (
+      <div>
+        {newList.map(item => {
+          return <div
+            key={item.citysName}
+            className={styles.cityItem}
+            onClick={() => handleCityChoose(item.citysName)}>
+            {item.citysName}
+          </div>
+        })}
+      </div>
+    )
+  }
+
+
 
   return (
     <div>
       <div>{renderStatusBar()}</div>
+      <Input placeholder='搜索热门城市' onPressEnter={handleSearch} />
+      <div>
+        <div className={styles.localTitle}>当前城市</div>
+        <div className={styles.currentLocal}>{currentLocal}</div>
+      </div>
       {renderCity()}
+
     </div>
   );
 };
 
-export default ChooseCity;
+export default observer(ChooseCity);
